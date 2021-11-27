c98b8535c99e37b4ff525640fc7a6c4a4671c5fa
Adrian Ichim <adrian.ichim@blueprism.com>
2021-11-27 04:50:29 +0000

    Finished module4

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,7 +1,10 @@
 from typing import List
-from pathlib import path
+from pathlib import Path
 import shutil
-
+import sys
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 
 class Parser:
     extensions: List[str] = []
@@ -9,22 +12,40 @@ class Parser:
         return (extension in self.extensions)
 
     def parse(self, path: Path, source: Path, dest: Path):
-        raise NotImplementedError()
+        raise NotImplementedError
 
     def read(self, path):
-        with path.open() as file:
+        with open(path, "r") as file:
             return (file.read())
 
     def write(self, path, dest, content, ext = ".html"):
-        full_path = self.dest / path.with_suffix(ext).name
-        with full_path.open() as file:
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
             file.write(content)
 
     def copy(self, path, source, dest):
-        copy2(path, (dest.relative_to(source)))
-
+        shutil.copy2(path, dest / path.relative_to(source))
 
 class ResourceParser(Parser):
-    extensions: List(str) = [".jpg", ".png", "gif", ".css", ".html"]
+    extensions: List(str) = [".jpg", ".png", ".gif", ".css", ".html"]
     def parse(self, path: Path, source: Path, dest: Path):
         super().copy(path, source, dest)
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name = "html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+        
--- ssg/site.py
+++ ssg/site.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 
 class Site:
@@ -24,8 +25,12 @@ class Site:
                 return parser
 
     def run_parser(self, path):
-        self.parser = self.load_parser(path.suffix)
-        if self.parser is not None:
-            self.parser.parse(path, self.source, self.dest)
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
         else:
-            print("Not implemented")
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
